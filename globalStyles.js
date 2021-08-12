1ebba08d7df6639553147d1a64cd848a363e5dd5
paolaforer0 <andreaforero502@gmail.com>
2021-08-12 00:49:26 -0500

    agregué el archivo globalStyles para importar fuentes y estilos constantes

---
+++ globalStyles.js
@@ -0,0 +1,24 @@
+import {createGlobalStyle} from 'styled-components'
+import MonserratBlack from './utils/fonts/Monserrat-Black.ttf'
+import MonserratBold from './utils/fonts/Monserrat-Bold.ttf'
+import MonserratExtraBold from './utils/fonts/Monserrat-ExtraBold.ttf'
+import MonserratMedium from './utils/fonts/Monserrat-Medium.ttf'
+import MonserratRegular from './utils/fonts/Monserrat-Regular.ttf'
+import MonserratSemiBold from './utils/fonts/Monserrat-SemiBold.ttf'
+import RobotoBlack from './utils/fonts/Roboto-Black.ttf'
+import RobotoBold from './utils/fonts/Roboto-Bold.ttf'
+import RobotoMedium from './utils/fonts/Roboto-Medium.ttf'
+import RobotoRegular from './utils/fonts/Roboto-Regular.ttf'
+
+const GlobalStyles = createGlobalStyle `
+
+body{
+    margin-top: 0%;
+    margin-bottom: 0%;
+    margin-left: 0%;
+    margin-right: 0%;
+
+}
+
+`
+export default GlobalStyles;
